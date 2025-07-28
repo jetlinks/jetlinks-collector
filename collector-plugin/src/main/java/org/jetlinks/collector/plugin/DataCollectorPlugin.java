97c8ec98a996786a106cd5c444e537f2e94c53fc
zhouhao <zh.sqy@qq.com>
2025-07-28 15:11:02 +0800

    refactor: 优化数采

--- collector-plugin/src/main/java/org/jetlinks/collector/plugin/DataCollectorPlugin.java
+++ collector-plugin/src/main/java/org/jetlinks/collector/plugin/DataCollectorPlugin.java
@@ -7,26 +7,26 @@ import org.jetlinks.plugin.core.PluginContext;
 import org.jetlinks.plugin.core.PluginType;
 
 /**
- * 数据采集插件,继承此抽象类,平台将调用{@link CollectorPlugin#createProviders()}方法注册数采提供商到平台
+ * 数据采集插件,继承此抽象类,平台将调用{@link DataCollectorPlugin#createProviders()}方法注册数采提供商到平台
  *
  * @author zhouhao
  * @since 1.0.0
  */
-public abstract class CollectorPlugin extends AbstractPlugin {
+public abstract class DataCollectorPlugin extends AbstractPlugin {
 
     public static final PluginType TYPE = new PluginType() {
         @Override
         public String getId() {
-            return "collector-v2";
+            return "data_collector";
         }
 
         @Override
         public String getName() {
-            return LocaleUtils.resolveMessage("message.plugin.collector.name","数据采集v2.0");
+            return LocaleUtils.resolveMessage("message.plugin.collector.name","数据采集");
         }
     };
 
-    public CollectorPlugin(String id, PluginContext context) {
+    public DataCollectorPlugin(String id, PluginContext context) {
         super(id, context);
     }
 
